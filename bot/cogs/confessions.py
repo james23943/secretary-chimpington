566bd9b5c3ff08b9596c9e1061e61da1d423142a
james <153340021+james23943@users.noreply.github.com>
2024-12-09 20:28:39 +1000

    v3.3

--- bot/cogs/confessions.py
+++ bot/cogs/confessions.py
@@ -4,6 +4,20 @@ from discord import app_commands
 import json
 from pathlib import Path
 
+class ConfessionReplyButton(discord.ui.Button):
+    def __init__(self):
+        super().__init__(label="Reply", style=discord.ButtonStyle.blurple)
+    
+    async def callback(self, interaction: discord.Interaction):
+        thread_name = f"Replies to {interaction.message.embeds[0].title}"
+        thread = await interaction.message.create_thread(name=thread_name)
+        await interaction.response.send_message(f"Created thread for replies!", ephemeral=True)
+
+class ConfessionView(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+        self.add_item(ConfessionReplyButton())
+
 class Confessions(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -47,7 +61,7 @@ class Confessions(commands.Cog):
             "Your confession has been submitted!", 
             ephemeral=True
         )
-        await interaction.channel.send(embed=embed)
+        await interaction.channel.send(embed=embed, view=ConfessionView())
 
 async def setup(bot):
     await bot.add_cog(Confessions(bot))
